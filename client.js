28e72b045171b0cde2d47cf03ded81447888e678
Davi Dantas Soares <soares.davi@escolar.ifrn.edu.br>
2022-01-31 10:06:36 -0300

    atualizações e adição de comentário

--- client.js
+++ client.js
@@ -1,7 +1,12 @@
+/*
+Para o node, temos um módulo chamado soap, que permite criarmos um client que consegue 
+realizar as invocações dos métodos
+*/
 const soap = require('soap')
 const url = 'http://localhost:8000/?wsdl'
 
 
+// Aqui se tem a criação do client soap e a invocação dos métodos que foram definidos no servidor
 soap.createClient(url, function(err, client) {
     client.fibonacci({n:20}, function(err, result) {
         console.log(result)
--- server.py
+++ server.py
@@ -6,8 +6,15 @@ from spyne.server.wsgi import WsgiApplication
 from math import isqrt
 
 
+'''
+Service base é uma classe base que define o webservice
+'''
 class matematica(ServiceBase):
 
+    '''
+    O decorador @rpc faz com que um método seja disponibilizado para invocação remota.
+    Nele, definimos o tipo de entrada e o tipo da saída do método
+    '''
     @rpc(Integer, _returns=Iterable(Integer))
     def crivo_eratostenes(ctx, n):
         if n <= 2:
@@ -42,27 +49,37 @@ class matematica(ServiceBase):
         return nums
     
         
-
-
-
-
-application = Application([matematica], 'soaps',
+'''
+Application é uma classe do spyne que nos permite definir como vai ser a comunicação 
+entre servidor e client, que no caso é o Soap11.
+Nela também é necessário incluir as classes que vão ser disponibilizadas, nesse caso 'matematica'
+e um nome para a aplicação
+'''
+application = Application([matematica], 'soap',
                           in_protocol=Soap11(validator='lxml'),
                           out_protocol=Soap11())
 
 wsgi_application = WsgiApplication(application)
 
 
-if __name__ == '__main__':
-    import logging
 
-    from wsgiref.simple_server import make_server
+import logging
 
-    logging.basicConfig(level=logging.DEBUG)
-    logging.getLogger('spyne.protocol.xml').setLevel(logging.DEBUG)
+from wsgiref.simple_server import make_server
 
-    logging.info("listening to http://127.0.0.1:8000")
-    logging.info("wsdl is at: http://localhost:8000/?wsdl")
+'''
+A biblioteca logging é uma forma simples de conseguirmos ter um log de tudo que acontece no servidor
+'''
 
-    server = make_server('127.0.0.1', 8000, wsgi_application)
-    server.serve_forever()
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('spyne.protocol.xml').setLevel(logging.DEBUG)
+
+logging.info("Ouvindo a porta http://127.0.0.1:8000")
+logging.info("wsdl em : http://localhost:8000/?wsdl")
+
+'''
+o simple server outra biblioteca que permite inicialização de um servidor e 
+o funcionamento dele indefinidamente
+'''
+server = make_server('127.0.0.1', 8000, wsgi_application)
+server.serve_forever()
